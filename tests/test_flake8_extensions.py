71c200a44da72c91d2020b7613b80e3148a9776c
Dan Birman <danbirman@gmail.com>
2024-08-30 11:41:14 -0700

    tests: additional tests
    These are failing in other repos, but the tests are passing. Not sure what's going on

--- tests/test_flake8_extensions.py
+++ tests/test_flake8_extensions.py
@@ -43,16 +43,6 @@ class MyModel(BaseModel):
         self.assertEqual(len(errors), 1)
         self.assertEqual(errors[0][2], "PF002 Field 'name' should use '...' for required fields")
 
-    def test_correct_required_with_value(self):
-        code = """
-from pydantic import BaseModel, Field
-
-class MyModel(BaseModel):
-    name: str = Field("value")
-"""
-        errors = self.check_code(code)
-        self.assertEqual(len(errors), 0)
-
     def test_required_field_blank(self):
         code = """
 from pydantic import BaseModel, Field
@@ -120,6 +110,29 @@ from pydantic import BaseModel
 
 class MyModel(BaseModel):
     stimulus_devices: List[int] = Field(default=[])
+"""
+        errors = self.check_code(code)
+        self.assertEqual(len(errors), 0)
+
+    def test_correct_required_with_value(self):
+        code = """
+from pydantic import BaseModel, Field
+
+class MyModel(BaseModel):
+    name: str = Field("value")
+"""
+        errors = self.check_code(code)
+        self.assertEqual(len(errors), 0)
+
+    def test_correct_required_with_object(self):
+        code = """
+from pydantic import BaseModel, Field
+
+class ValueModel(BaseModel):
+    data: int = Field(...)
+
+class MyModel(BaseModel):
+    value: ValueModel = Field(ValueModel(int=1))
 """
         errors = self.check_code(code)
         self.assertEqual(len(errors), 0)
